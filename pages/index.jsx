2b53f46ea7b5c55b35a921a0a497d6372a2c2d4f
Vishal Rana <vishal.ch.36@gmail.com>
2022-10-28 13:17:18 +0530

    Added share and orientation sensor

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,23 @@
 import Scene from "../src/app/box";
+import Compass from "../src/app/compass";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
+  const shareData = {
+    title: "Vishal Rana",
+    text: "Trying the default share of devices, here's my portfolio link",
+    url: "https://vishalrana.in",
+  };
+  const shareit = () => {
+    if (navigator.share) {
+      navigator
+        .share(shareData)
+        .then(() => console.log("Successful share"))
+        .catch((error) => console.log("Error sharing", error));
+    } else {
+      alert("Sharing Not Supported on " + navigator.platform + "!");
+    }
+  };
   const vibrate = () => {
     if (navigator.vibrate) {
       navigator.vibrate([200, 100, 200]);
@@ -17,11 +33,35 @@ export default function Home() {
         </h1>
 
         <Scene />
-        <div className={styles.grid}>
-          <div className={styles.card}>
-            <button className="btn btn-success" onClick={vibrate}>
-              Vibrate
-            </button>
+        <div className="container">
+          <h3 className="text-center">APIs</h3>
+          <div className="row">
+            <div className="col-md-3">
+              <div className={styles.card}>
+                <div className="text-center">
+                  <button className="btn btn-success" onClick={shareit}>
+                    Share
+                  </button>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-3">
+              <div className={styles.card}>
+                <div className="text-center">
+                  <button className="btn btn-success" onClick={vibrate}>
+                    Vibrate
+                  </button>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-3">
+              <div className={styles.card}>
+                <div className="text-center">
+                  <h5>Orientation</h5>
+                  {<Compass />}
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </main>
--- src/app/compass.jsx
+++ src/app/compass.jsx
@@ -8,7 +8,27 @@ function Box(props) {
   const [hovered, setHover] = useState(false);
   const [active, setActive] = useState(false);
   // Subscribe this component to the render-loop, rotate the mesh every frame
-  useFrame((state, delta) => (mesh.current.rotation.x += 0.01));
+
+  useFrame(
+    (state, delta) => {
+      if (window.DeviceOrientationEvent) {
+        window.addEventListener(
+          "deviceorientation",
+          (event) => {
+            const rotateDegrees = event.alpha; // alpha: rotation around z-axis
+            const leftToRight = event.gamma; // gamma: left to right
+            const frontToBack = event.beta; // beta: front back motion
+            mesh.current.rotation.x = frontToBack / 90;
+            mesh.current.rotation.y = leftToRight / 90;
+            mesh.current.rotation.z = rotateDegrees / 90;
+            // handleOrientationEvent(frontToBack, leftToRight, rotateDegrees);
+          },
+          true
+        );
+      }
+    }
+    // mesh.current.rotation.x += 0.01
+  );
   // Return view, these are regular three.js elements expressed in JSX
   return (
     <mesh
@@ -19,19 +39,18 @@ function Box(props) {
       onPointerOver={(event) => setHover(true)}
       onPointerOut={(event) => setHover(false)}
     >
-      <boxGeometry args={[1, 1, 1]} />
+      <boxGeometry args={[2, 3, 1]} />
       <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
     </mesh>
   );
 }
 
-export default function Scene() {
+export default function Compass() {
   return (
     <Canvas>
       <ambientLight />
       <pointLight position={[10, 10, 10]} />
-      <Box position={[-1.2, 0, 0]} />
-      <Box position={[1.2, 0, 0]} />
+      <Box position={[0, 0, 0]} />
     </Canvas>
   );
 }
